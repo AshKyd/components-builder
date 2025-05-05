65790683f6493562e1c9e414463214609ec03891
Ash Kyd <kyd.ashley@abc.net.au>
2025-05-05 16:02:29 +1000

    initial commit

---
+++ src/lib/GoogleDocScrollyteller/GoogleDocScrollyteller.stories.svelte
@@ -0,0 +1,24 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import GoogleDocScrollyteller from "./GoogleDocScrollyteller.svelte";
+  import BuilderStyleRoot from "$lib/BuilderStyleRoot/BuilderStyleRoot.svelte";
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: "Example/GoogleDocScrollyteller",
+    component: GoogleDocScrollyteller,
+    tags: ["autodocs"],
+    argTypes: {},
+    args: {},
+    decorators: [() => BuilderStyleRoot],
+  });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story
+  name="Primary"
+  args={{
+    name: "myscrollyteller",
+    markerName: "mark",
+  }}
+/>
---
+++ src/lib/ScreenshotTool/ScreenshotTool.stories.svelte
@@ -0,0 +1,26 @@
+<script module>
+  import { defineMeta } from "@storybook/addon-svelte-csf";
+  import ScreenshotTool from "./ScreenshotTool.svelte";
+  import BuilderStyleRoot from "$lib/BuilderStyleRoot/BuilderStyleRoot.svelte";
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: "Example/ScreenshotTool",
+    component: ScreenshotTool,
+    tags: ["autodocs"],
+    argTypes: {},
+    args: {},
+    decorators: [() => BuilderStyleRoot],
+  });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story
+  name="Primary"
+  args={{
+    // Prefixes to look for in the pasted document
+    prefixes: { Marker: "#mark" },
+    // Do something with the markers before screenshotting (e.g. turn off animations)
+    parseMarker: (markerString) => "markNewMarkerString",
+  }}
+/>
