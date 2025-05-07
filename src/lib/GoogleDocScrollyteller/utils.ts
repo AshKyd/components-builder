2e8d4d3f99063b35216f703d42dd6ac9caa16cd0
Ash Kyd <kyd.ashley@abc.net.au>
2025-05-07 11:14:44 +1000

    bug: pass in own scrollyteller

--- src/lib/GoogleDocScrollyteller/utils.ts
+++ src/lib/GoogleDocScrollyteller/utils.ts
@@ -1,11 +1,9 @@
-import { loadScrollyteller } from '@abcnews/svelte-scrollyteller';
-
 function mountTextToMountEl(mountText: string) {
-  const mountEl = document.createElement('div');
+  const mountEl = document.createElement("div");
 
-  mountEl.setAttribute('data-mount', '');
-  mountEl.setAttribute('data-component', 'Anchor');
-  mountEl.setAttribute('id', mountText.slice(1));
+  mountEl.setAttribute("data-mount", "");
+  mountEl.setAttribute("data-component", "Anchor");
+  mountEl.setAttribute("id", mountText.slice(1));
 
   return mountEl;
 }
@@ -22,30 +20,31 @@ function mountTextToMountEl(mountText: string) {
  */
 export async function loadData({
   name,
-  className = 'u-full',
-  markerName = 'mark',
+  className = "u-full",
+  markerName = "mark",
   url,
-  preprocessCoreEl = el => el,
-  postprocessScrollytellerDefinition = a => a
+  preprocessCoreEl = (el) => el,
+  postprocessScrollytellerDefinition = (a) => a,
+  loadScrollyteller,
 }) {
   if (!url) {
     return;
   }
 
-  const pubURL = url.replace(/\/[^/]+?$/, '/pub');
-  const html = await fetch(pubURL).then(response => response.text());
-  const dom = new DOMParser().parseFromString(html, 'text/html');
-  const body = dom.querySelector('#contents > div');
+  const pubURL = url.replace(/\/[^/]+?$/, "/pub");
+  const html = await fetch(pubURL).then((response) => response.text());
+  const dom = new DOMParser().parseFromString(html, "text/html");
+  const body = dom.querySelector("#contents > div");
 
-  const title = dom.querySelector('title')?.textContent;
+  const title = dom.querySelector("title")?.textContent;
 
   if (!body) {
-    throw new Error('Body not found');
+    throw new Error("Body not found");
   }
 
-  Array.from(body.querySelectorAll('*')).forEach(el => {
-    el.removeAttribute('class');
-    el.removeAttribute('id');
+  Array.from(body.querySelectorAll("*")).forEach((el) => {
+    el.removeAttribute("class");
+    el.removeAttribute("id");
   });
 
   const coreEls: Element[] = Array.from(body.children).map(preprocessCoreEl);
@@ -53,19 +52,19 @@ export async function loadData({
   const coreText = coreEls.reduce<string>((memo, el) => {
     const text = String(el.textContent).trim();
 
-    memo = `${memo}\n${text ? `\n${text}` : ''}`;
+    memo = `${memo}\n${text ? `\n${text}` : ""}`;
 
     return memo;
-  }, '');
+  }, "");
 
   const coreHTML = coreEls.reduce<string>((memo, el) => {
     const text = String(el.textContent).trim();
     const html = el.outerHTML;
 
-    memo = `${memo}${text ? `${html}` : ''}`;
+    memo = `${memo}${text ? `${html}` : ""}`;
 
     return memo;
-  }, '');
+  }, "");
 
   const { scrollytellingEls } = coreEls.reduce<{
     hasBegun: boolean;
@@ -80,19 +79,22 @@ export async function loadData({
 
       const text = String(el.textContent).trim();
 
-      if (text.indexOf('#remove') === 0) {
+      if (text.indexOf("#remove") === 0) {
         memo.isRemoving = true;
-      } else if (text.indexOf('#endremove') === 0) {
+      } else if (text.indexOf("#endremove") === 0) {
         memo.isRemoving = false;
-      } else if (text.indexOf('#') === 0) {
-        if (text.indexOf(`#scrollyteller${name ? `NAME${name}` : ''}`) === 0 && !memo.hasBegun) {
+      } else if (text.indexOf("#") === 0) {
+        if (
+          text.indexOf(`#scrollyteller${name ? `NAME${name}` : ""}`) === 0 &&
+          !memo.hasBegun
+        ) {
           memo.hasBegun = true;
-        } else if (text.indexOf('#endscrollyteller') === 0) {
+        } else if (text.indexOf("#endscrollyteller") === 0) {
           memo.hasEnded = true;
         }
 
         memo.scrollytellingEls.push(mountTextToMountEl(text));
-      } else if (!memo.hasBegun || memo.isRemoving || text === '') {
+      } else if (!memo.hasBegun || memo.isRemoving || text === "") {
         // skip
       } else {
         memo.scrollytellingEls.push(el);
@@ -104,26 +106,30 @@ export async function loadData({
       hasBegun: false,
       hasEnded: false,
       isRemoving: false,
-      scrollytellingEls: []
-    }
+      scrollytellingEls: [],
+    },
   );
 
-  const container = document.createElement('div');
+  const container = document.createElement("div");
 
-  scrollytellingEls.forEach(scrollytellingEl => container.appendChild(scrollytellingEl));
+  scrollytellingEls.forEach((scrollytellingEl) =>
+    container.appendChild(scrollytellingEl),
+  );
   document.body.appendChild(container);
   let scrollytellerDefinition = loadScrollyteller(name, className, markerName);
 
   document.body.removeChild(container);
 
   if (postprocessScrollytellerDefinition) {
-    scrollytellerDefinition = postprocessScrollytellerDefinition(scrollytellerDefinition);
+    scrollytellerDefinition = postprocessScrollytellerDefinition(
+      scrollytellerDefinition,
+    );
   }
 
   return {
     title,
     coreText,
     coreHTML,
-    scrollytellerDefinition
+    scrollytellerDefinition,
   };
 }
